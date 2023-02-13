f7396669bff905ad874dd8344c037a988bf8c644
HCH <2762312941@qq.com>
2023-02-13 23:38:11 +0800

    table entry

---
+++ databases/entry.go
@@ -0,0 +1,60 @@
+package databases
+
+import (
+	"simple_douyin/biz/model/common"
+
+	"gorm.io/gorm"
+)
+
+type UserEntry struct {
+	gorm.Model
+	UID           int64  `gorm:"column:uid"`
+	Name          string `gorm:"column:name"`
+	FollowCount   int64  `gorm:"column:follow_count"`
+	FollowerCount int64  `gorm:"column:follower_count"`
+	IsFollow      bool
+}
+
+func User2UserEntry(u *common.User) *UserEntry {
+	return &UserEntry{}
+}
+
+func UserEntry2User(ue *UserEntry) *common.User {
+	return &common.User{
+		ID:            ue.UID,
+		Name:          ue.Name,
+		FollowCount:   ue.FollowCount,
+		FollowerCount: ue.FollowerCount,
+		IsFollow:      ue.IsFollow,
+	}
+}
+
+type VideoEntry struct {
+	gorm.Model
+	VID           int64
+	Author        UserEntry `gorm:"ForeignKey:UserEntryID"`
+	PlayURL       string
+	CoverURL      string
+	FavoriteCount int64
+	CommentCount  int64
+	IsFavorite    bool
+	Title         string
+	UserEntryID   int64
+}
+
+func Video2VideoEntry(v *common.Video) *VideoEntry {
+	return &VideoEntry{}
+}
+
+func VideoEntry2Video(ve *VideoEntry) *common.Video {
+	return &common.Video{
+		ID:            ve.VID,
+		Author:        UserEntry2User(&ve.Author),
+		PlayURL:       ve.PlayURL,
+		CoverURL:      ve.CoverURL,
+		FavoriteCount: ve.FavoriteCount,
+		CommentCount:  ve.CommentCount,
+		IsFavorite:    ve.IsFavorite,
+		Title:         ve.Title,
+	}
+}
--- databases/video_db.go
+++ databases/video_db.go
@@ -2,28 +2,32 @@ package databases
 
 import (
 	"fmt"
-	"simple_douyin/biz/model/common"
 
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
-type Video struct {
-	gorm.Model
-	common.Video
-}
-
 func Init() {
 	fmt.Println("233333333333")
-	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	db, err := gorm.Open(sqlite.Open("videos.db"), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
 	}
-	db.AutoMigrate(&Video{})
-	db.Create(&Video{ID: 123})
-	db.Create(&Video{ID: 123})
-	var video Video
-	db.First(&video, 1) // 根据整型主键查找
-	fmt.Println(video)
-	db.First(&video, "id = ?", 123) // 查找 code 字段值为 D42 的记录
+	db.AutoMigrate(&VideoEntry{})
+
+	// Create
+	db.Create(&VideoEntry{})
+	// db.Create(&UserEntry{Model: gorm.Model{ID: 1}, UID: 456, Name: "HCH2"})
+	// db.Create(&UserEntry{Model: gorm.Model{ID: 2}, UID: 456, Name: "HCH3"})
+	// fmt.Println("err: ", result.Error)
+	// user := UserEntry{Model: gorm.Model{ID: 100}}
+	// fmt.Printf("%+v\n", user)
+	// fmt.Println(user.Model.ID)
+	// user = UserEntry{}
+	// db.First(&user, 1) // 根据整型主键查找
+	// fmt.Println("0", user)
+	// db.First(&user, "name = ?", "HCH") // 查找 code 字段值为 D42 的记录
+	// fmt.Println("1", user)
+	// db.First(&user, "name = ?", "HCH3") // 查找 code 字段值为 D42 的记录
+	// fmt.Println("2", user)
 }
