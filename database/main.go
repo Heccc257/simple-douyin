6ee521d2b71055edc5027009f00926388bb2a6db
HCH <2762312941@qq.com>
2023-02-14 23:05:53 +0800

    database init

--- database/main.go
+++ database/main.go
@@ -2,62 +2,9 @@ package database
 
 import (
 	"fmt"
-	"log"
 	"simple_douyin/biz/model/common"
 	"time"
-
-	"gorm.io/driver/sqlite"
-	"gorm.io/gorm"
 )
-
-var db *gorm.DB
-var err error
-
-func Init() {
-	db, err = gorm.Open(sqlite.Open("database/douyin.db"), &gorm.Config{})
-	if err != nil {
-		panic("failed to connect database")
-	}
-
-	// db.Migrator().DropTable("user_entries", "video_entries")
-
-	// 创建表之后会默认添加一个unknown用户
-	// 以及两个作者为unkn的视频
-	if exist := db.Migrator().HasTable("user_entries"); !exist {
-		if err := db.AutoMigrate(&UserEntry{}); err != nil {
-			log.Fatal(err)
-		}
-		// 创建一个默认用户
-		db.Create(&UserEntry{
-			UID:  0,
-			Name: "unknown",
-		})
-	}
-	if exist := db.Migrator().HasTable("video_entries"); !exist {
-		if err := db.AutoMigrate(&VideoEntry{}); err != nil {
-			log.Fatal(err)
-		}
-		videos := [2]VideoEntry{}
-		db.Create(&videos)
-	}
-
-	log.Println("database initialized")
-	tables, _ := db.Migrator().GetTables()
-	log.Println("tables: ", tables)
-
-	// var user UserEntry
-	// result := db.First(&user, 1)
-	// fmt.Println("````````````````````````")
-	// fmt.Println("res: ", result.Error)
-	// fmt.Println("user: ", user)
-	// fmt.Println("````````````````````````")
-
-	// var vs []VideoEntry
-	// db.Find(&vs)
-	// fmt.Println("````````````````````````")
-	// fmt.Println("videos: ", vs)
-	// fmt.Println("````````````````````````")
-}
 
 func FindUserEntry(u *common.User) *UserEntry {
 	var ue UserEntry
